cfb72ff5a98d0487a0c6d9514e6da0fd1a777470
Januda Amaneth <jaariyasinghe2003@gmail.com>
2025-07-11 15:24:57 +0530

    Refactor Navbar Section by using Tailwind CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,29 +2,42 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="site-nav">
             <div>
-                <div className="site-logo">
+                <button>
                     <img
                         src="/images/icon-hamburger.svg"
-                        alt="hamburger-menu"
+                        alt=""
+                        className="hamburger-menu"
                     />
+                </button>
+                <a href="#home">
+                    <img src="/images/logo.svg" alt="site-logo" />
+                </a>
+                <div className="desktop-navbar">
+                    <ul>
+                        <li>
+                            <a href="#home" className="underline-hover">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#shop" className="underline-hover">
+                                Shop
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#about" className="underline-hover">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#contact" className="underline-hover">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <a href="#home">Willow Nest</a>
-                <ul>
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li>
-                        <a href="">Shop</a>
-                    </li>
-                    <li>
-                        <a href="">About</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                </ul>
             </div>
         </nav>
     );
